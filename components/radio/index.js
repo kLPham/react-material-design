c21db13beb70f21cead982f6d7985eb7e00c0dee
Miguel Amaro <miguel_f_amaro@homedepot.com>
2017-07-23 00:39:26 -0700

    refactor radio

--- components/radio/index.js
+++ components/radio/index.js
@@ -1,74 +1,49 @@
 import '@material/radio/dist/mdc.radio.css';
-import classNames from 'classnames';
-import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Set as ImmutableSet } from 'immutable';
+import React, { Component } from 'react';
+import { MDCRadio } from '@material/radio';
 import { v4 } from 'uuid';
-import MDCRadio from './component';
-import FormField from '../formField';
-// TODO: componentWillReceiveProp lifecycle.
-// TODO: grouping radio buttons. children?
 
-class Radio extends PureComponent {
+// TODO: oncClick handle
+class Radio extends Component {
     static propTypes = {
-        alignEnd: PropTypes.bool,
-        checked: PropTypes.bool,
-        disabled: PropTypes.bool,
         label: PropTypes.string,
-    };
-    static defaultProps = {
-        alignEnd: false,
-        checked: false,
-        disabled: false,
-        label: '',
-    };
-    constructor(props) {
-        super(props);
-        this.state = {
-            checked: props.checked,
-            classes: new ImmutableSet(),
-            disabled: props.disabled,
-            label: props.label,
-        };
+        disable: PropTypes.bool,
+        checked: PropTypes.bool,
+        name: PropTypes.string,
     }
     componentDidMount() {
-        const { disabled } = this.props;
-        this.foundation = new MDCRadio(this);
-
-        if (disabled) {
-            this.foundation.setDisabled(disabled);
-        }
+        const { disable, checked } = this.props;
+        this.radio = new MDCRadio(this.mainRoot);
+        this.radio.foundation_.setDisabled(disable);
+        this.radio.foundation_.setChecked(checked);
     }
-    handleChange=(e) => {
-        const checked = e.target.value;
-        this.foundation.setChecked(checked);
-        this.setState({
-            checked,
-        });
-    };
     render() {
-        const { checked, classes, disabled, label } = this.state;
-        const { alignEnd } = this.props;
+        const { label, name } = this.props;
         const id = v4();
+
         return (
-            <FormField alignEnd={alignEnd}>
-                <div className={classNames('mdc-radio', classes.toJS().join(' '))} >
+            <div>
+                <div ref={(d) => { this.mainRoot = d; }} className="mdc-radio">
                     <input
-                        checked={checked}
                         className="mdc-radio__native-control"
-                        disabled={disabled}
-                        id={`mdc-radio--${id}`}
-                        onChange={e => this.handleChange(e)}
                         type="radio"
+                        id={`mdc-radio--${id}`}
+                        name={name}
                     />
                     <div className="mdc-radio__background">
                         <div className="mdc-radio__outer-circle" />
                         <div className="mdc-radio__inner-circle" />
                     </div>
                 </div>
-                {label && <label id={`mdc-radio-label--${id}`} htmlFor={`mdc-radio--${id}`}>{label}</label>}
-            </FormField>
+                <label
+                    id={`mdc-radio-label--${id}`}
+                    htmlFor={`mdc-radio--${id}`}
+                >
+                    {label}
+                </label>
+            </div>
         );
     }
-}
+          }
 export default Radio;
--- examples/docs/src/Containers/SelectionControls.js
+++ examples/docs/src/Containers/SelectionControls.js
@@ -9,12 +9,15 @@ const SelectionControls = () =>
       <Checkbox label="My Checkbox Label" />
       <Checkbox alignEnd label="Align end: My Checkbox Label" />
       <h2>Radio Buttons</h2>
-      <Radio disabled label="Disabled Radio 1" />
+      <Radio disable label="Disabled Radio 1" />
       <Radio label="Radio 2" />
       <Radio alignEnd label="Align End: Radio 3" />
+      <Radio checked label="Checked: Radio 4" />
+      <Radio name="radios" label="Checked: Radio 4" />
+
       <h2>Switches</h2>
       <Switch label="off/on" />
-      <Switch disabled label="disabled" />
+      <Switch disable label="disable" />
       <h2>Selects</h2>
       <Select />
       <h3>Disabled</h3>
